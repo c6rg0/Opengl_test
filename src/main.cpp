ac700d23f68ee3860992ebca53fb691b0d3cace9
c6rg0 <gdrozbik08@outlook.com>
2025-08-15 15:58:17 +0100

    Added GLAD, window scalability, and more

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 //main.cpp
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 using namespace std;
 
-int main(void) {
+void processInput(GLFWwindow *window)
+{
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main() {
 	// Initialize GLFW
 	if (!glfwInit()){
 		cerr << "GLFW initialization failed!" << endl;
@@ -12,25 +24,37 @@ int main(void) {
 	
 	GLFWwindow* window;
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
 	/* Create a windowed mode window and its OpenGl context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
 	{
+		cout << "Failed to create GLFW window" << endl;
 		glfwTerminate();
 		return -1;
 	}
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to initialize GLAD" << endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 640, 480);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
 	
 	/*Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
-	{
+	{	
+
+		processInput(window);
+
 		/* Render here */
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		/* Swap front and back buffers */
